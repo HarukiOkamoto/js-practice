0f57cafd5da076a1c389cc676dd24e17f50d86fe
HarukiOkamoto <alice.159357@icloud.com>
2023-10-03 23:34:34 +0900

    10/3

--- check-check/js/sctipt.js
+++ check-check/js/sctipt.js
@@ -3,9 +3,11 @@ const btn = document.querySelector('#btn');
 
 //イベント change：変化が起きたとき
 isAgreed.addEventListener('change', () => {
-    if (isAgreed.checked === true) {
-        btn.disabled = false;
-    } else {
-        btn.disabled = true;
-    }
+
+    btn.disabled = !isAgreed.checked;
+    // if (isAgreed.checked === true) {
+    //     btn.disabled = false;
+    // } else {
+    //     btn.disabled = true;
+    // }
 });
---
+++ scroll/js/sctipt.js
@@ -0,0 +1,13 @@
+const getScrollPercent = () => {
+    const scrolled = window.scrollY;
+    console.log(`${scrolled} スクロールされました`);
+
+    //ページ全体の高さ
+    const pageHeight = document.documentElement.scrollHeight;
+    //表示領域の高さ
+    const viewHeight = document.documentElement.clientHeight;
+
+    console.log(`ページの高さ：${pageHeight}、表示領域の高さ：${viewHeight}`);
+}
+
+window.addEventListener('scroll', getScrollPercent);
