9990f0cf5f3a44940128f947cbe2489c0a748416
HarukiOkamoto <alice.159357@icloud.com>
2023-10-10 01:26:05 +0900

    10/10

--- animation/js/sctipt.js
+++ animation/js/sctipt.js
@@ -1,14 +1,13 @@
 const head = document.querySelector("#heading");
 
 const keyframes = {
-    opacity: [0, 1],
-    translate: ['0 50px', 0],
+    color: ['transparent', '#fff'],
+    backgroundPosition: ['100% 0', '0 0'],
 };
 
 const options = {
-    duration: 2000,
+    duration: 1000,
     easing: 'ease',
 };
-
 
 head.animate(keyframes, options);
